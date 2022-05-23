f06425a6dd2d44c296fc159b67f8ca71e1ac0cc8
Sara Alghamdi <SaraSaeed.gh@gmail.com>
2022-05-23 16:44:43 +0300

    initial commit

---
+++ In Class/script.js
@@ -0,0 +1,13 @@
+
+const arr1=["a","b","c"];
+const arr2=[];
+let newItem;
+
+function push(arr,newItem ){
+        arr[arr.length]=newItem;
+        return a;
+    }
+
+    console.log(push(arr1,"d"));
+    console.log(push(arr2,"a"));
+
